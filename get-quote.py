38a27295f0d006255c0f1df8e4573491632f5b80
Simao Pedro Silva <84675129+XOGUMeMAIL@users.noreply.github.com>
2021-05-25 21:28:15 +0100

    Final update to get-quote.py
    Indentation fully corrected using PyCharm

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,12 @@
 def frases():
-    print("A day of Glory!");
+    print("A day of Glory!")
 
-    f = open("quotes.txt");
-    quotes = f.readlines();
-    f.close();
+    f = open("quotes.txt")
+    quotes = f.readlines()
+    f.close()
 
-    print(quotes[0]);
+    print(quotes[0])
 
+    
 if __name__ == "__frases__":
-    frases();
+    frases()
